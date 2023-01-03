9d815f8316b28f34b2c74d8258ee08fae6ffc0d8
Patrick Vossler <patrick.vossler18@gmail.com>
2023-01-03 14:45:57 -0500

    use Jacob's one-liner for finding skipped chunks

--- examples/make-an-r-package-from-bookdown/frombookdown/tests/testthat/tests.R
+++
@@ -1,6 +0,0 @@
-# Generated from _main.Rmd: do not edit by hand  
-testthat::test_that("say_hello works", {
-  testthat::expect_equal(say_hello("Jacob"), "Hello Jacob!")
-  testthat::expect_equal(say_hello("Jacob", exclamation = FALSE), "Hello Jacob.")
-})
-
--- examples/make-an-r-package-with-rcpp/withrcpp/tests/testthat/tests.R
+++
@@ -1,6 +0,0 @@
-# Generated from create-withrcpp.Rmd: do not edit by hand  
-testthat::test_that("alternate_signs works", {
-  testthat::expect_equal(alternate_signs(1:3), c(1, -2, 3))
-  testthat::expect_equal(alternate_signs(c(-4, 2)), c(-4, -2))
-})
-
